6d5164ec05ad751eb8a45a28abc644130ecb99e3
zoker <nguyenthanhtin1994@gmail.com>
2023-10-27 14:07:22 +0700

    starting part 4

--- backend/models/note.js
+++ backend/models/note.js
@@ -1,28 +1,10 @@
 const mongoose = require('mongoose')
-const dotenv = require("dotenv");
-
-dotenv.config();
-
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
 
 const noteSchema = new mongoose.Schema({
     content: {
         type: String,
-        minLength: 5,
-        required: true
+        required: true,
+        minlength: 5
     },
     important: Boolean,
 })
